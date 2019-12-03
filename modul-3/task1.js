1dc8ee92abdb8c82a3f08ca8f405852d27c2f867
andrusha52 <andrushaes@gmail.com>
2019-12-03 12:27:18 +0200

    HW3

---
+++ modul-3/task1.js
@@ -0,0 +1,19 @@
+"use strict"
+
+
+
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+  };
+
+
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false ;
+
+for ( const key in user) {
+    console.log( key , user[key]);
+}
