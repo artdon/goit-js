df223fb3c486660144efdf169c7e9218bf6d48ae
andrusha52 <andrushaes@gmail.com>
2019-12-27 10:32:21 +0200

    js

--- modul-3/facebook.js
+++ modul-3/facebook.js
@@ -8,5 +8,15 @@ FB.getLoginStatus(function(response) {
 
 function statusChangeCallback(response){
     console.log(response);
+    FB.api(
+        '/me',
+        'GET',
+        {"fields":"id,name,profile_pic"},
+        function(response) {
+            console.log(response);
+            // Insert your code here
+        }
+      );
 }
+
 }
